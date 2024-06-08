639464189c422e25843c704c8eaf2b174094b842
Hello CoderBoy <mahmudhasan.hb@gmail.com>
2024-06-09 03:56:23 +0600

    added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const app = express()
 // const localStorage  = window.localStorage
 const port = 5000
-app.use(cors(['http://localhost:5173']))
+app.use(cors(['http://localhost:5173', 'https://darling-phoenix-126707.netlify.app']))
 app.use(express.json())
 
 app.get("/", async (req, res) => {
@@ -18,35 +18,54 @@ app.get("/", async (req, res) => {
 //   }
 
 
-async function Data() {
-    const res = await fetch('https://exam-server-7c41747804bf.herokuapp.com/carsList')
-    const allCars = await res.json()
-    // console.log(data.data);
-    
-    app.post("/rents", async (req, res) => {
-        const data = req.body
-        console.log(data);
+// async function Data() {
+//     // const res = await fetch('https://exam-server-7c41747804bf.herokuapp.com/carsList')
+//     // const allCars = await res.json()
+//     // console.log(data.data);
 
-        if(data?.duration_hour){
-            const totalCostOfHour = parseInt(data?.duration_hour)* parseInt(data?.rate?.hourly)
-            console.log(totalCostOfHour);
-            res.send(totalCostOfHour)
+// }
+
+// Data()
+
+
+
+app.post("/rents", async (req, res) => {
+    const data = req.body
+    console.log(data);
+
+    if (data?.duration_hour) {
+        const totalCostOfHour = parseInt(data?.duration_hour) * parseInt(data?.rate?.hourly)
+        console.log(totalCostOfHour);
+        if (totalCostOfHour >= data?.rate?.daily) {
+            const day = 1
+            const daily = data?.rate?.daily
+            const totalCostOfDay = data?.rate?.daily
+
+            const totalCost = { day, daily, totalCostOfDay }
+            return res.send(totalCost)
         }
-        else{
-            const totalCostOfDay = parseInt(data?.duration_day)* parseInt(data?.rate?.daily)
-            const totalCostOfWeek = parseInt(data?.duration_week)* parseInt(data?.rate?.weekly)
-            console.log(totalCostOfDay,totalCostOfWeek);
-            const totalCost = [totalCostOfDay,totalCostOfWeek]
-            res.send(totalCost)
+        const hour = data?.duration_hour
+        const hourly = data?.rate?.hourly
+        const totalCost = { totalCostOfHour, hour, hourly }
+        res.send(totalCost)
+    }
+    else {
+        const totalCostOfDay = parseInt(data?.duration_day) * parseInt(data?.rate?.daily)
+        const totalCostOfWeek = parseInt(data?.duration_week) * parseInt(data?.rate?.weekly)
+        const day = data?.duration_day
+        const week = data?.duration_week
+        const daily = data?.rate?.daily
+        const weekly = data?.rate?.weekly
+        console.log(totalCostOfDay, totalCostOfWeek);
+        const totalCost = { totalCostOfWeek, totalCostOfDay, weekly, daily, week, day }
+        // const totalCost = [totalCostOfDay, totalCostOfWeek, weekly, daily]
+        res.send(totalCost)
 
-        }
+    }
 
 
 
-    })
-}
-
-Data()
+})
 
 app.listen(port, () => {
     console.log(`Server is running at ${port}`);
